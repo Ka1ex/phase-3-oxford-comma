ed62f161e11357e0df5b0ecca44408d0b3e2eb9f
Kalex <kalexadamson@hotmail.com>
2021-08-29 18:12:25 -0500

    commit tree f17bc4441a2336172d176ca535ac07e27f762990

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,13 @@
 def oxford_comma(array)
+if array.length == 1 
+    array.join("")
+
+elsif array.length == 2 
+        array.join(" and ")
+elsif array.length >= 3
+    some_arr = array.pop
+    add_and = "and " + some_arr
+    array.push(add_and).join(", ")
+end
 
 end
